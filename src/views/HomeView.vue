526b5792740bf45368e5a18993c172797a8b3eb8
Felipe F. Morgado <ffmoliveira@ymail.com>
2023-09-13 17:54:45 +0200

    Last commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Homepage </h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem expedita asperiores nam fugit qui, et doloribus quis doloremque praesentium magnam laborum alias dolore ea velit perferendis necessitatibus facilis illum officia!</p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem expedita asperiores nam fugit qui, et doloribus quis doloremque praesentium magnam laborum alias dolore ea velit perferendis necessitatibus facilis illum officia!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -1,29 +1,35 @@
 <template>
-  <h1> Jobs </h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{name: 'JobDetails', params: { id: job.id}}">
-      <h2> {{ job.title }}</h2>
-    </router-link>
+  <h1>Jobs</h1>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p> Loading Jobs...</p>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            jobs: [
-                {title: 'UX Designer', id: 1, details: 'lorem'},
-                {title: 'Web Developer', id: 2, details: 'lorem'},
-                {title: 'Vue Developer', id: 3, details: 'lorem'}
-            ]
-        }
-    }
-
-}
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
-.job h2{
+.job h2 {
   background: #f4f4f4;
   padding: 20px;
   border-radius: 10px;
@@ -35,8 +41,7 @@ export default {
 .job h2:hover {
   background: #ddd;
 }
-.job a{
+.job a {
   text-decoration: none;
 }
-
 </style>
