8c783b79ed99eb607e06e52f9d18fbffc802fa42
Felipe F. Morgado <ffmoliveira@ymail.com>
2023-09-13 17:54:45 +0200

    Commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem expedita asperiores nam fugit qui, et doloribus quis doloremque praesentium magnam laborum alias dolore ea velit perferendis necessitatibus facilis illum officia!</p>
   </div>
 </template>
