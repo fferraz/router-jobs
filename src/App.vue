bd5cf6bf4d8cc61af27414857593b30653fecc7b
Felipe F. Morgado <ffmoliveira@ymail.com>
2023-09-13 18:27:20 +0200

    Router job page working

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,23 @@
   <router-view/>
 </template>
 
+<script>
+export default{
+  methods:{
+    redirect(){
+      this.$router.push({name: 'home'})
+    },
+    back(){
+      this.$router.go(-1)
+    },
+    forward(){
+      this.$router.go(1)
+    }
+  }
+}
+
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
